7111afd28e68630e8234c2f3e314c6f86136b85d
Serhii Yakymenko <64821812+serhii-edit@users.noreply.github.com>
2021-08-11 20:34:40 -0400

    Feat: Configured Gulp, On: Scss-BrowserSync-WatchReload

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require("gulp");
+var { src, dest, series, watch, on } = require("gulp");
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass')(require('sass'));
+
+function dist(done) {
+return src (["./src/**/*", "!src/scss/**"])
+  .pipe(dest("dist/"));
+  done();
+}
+
+// Static server
+function bs() {
+  browserSync.init({
+      server: {
+          baseDir: "./src/"
+      }
+  });
+  watch("./src/**/*.html").on('change', browserSync.reload);
+  watch("./src/css/**/*.css").on('change', browserSync.reload);
+  watch("./src/scss/**/*.scss", {ignoreInitial: false}, scss);
+  watch("./src/js/**/*.js", {ignoreInitial: false}).on("change", browserSync.reload);
+};
+
+function scss() {
+  return src("./src/scss/**/*.scss")
+  .pipe(sass())
+  .pipe(dest("./src/css/"))
+  .pipe(browserSync.stream());
+  // done();
+};
+
+
+
+exports.serve = series(bs);
+exports.build = series(dist);
+
+// Error
+// function callbackError(cb) {
+//   // `cb()` should be called by some async work
+//   cb(new Error('kaboom'));
+// }
+
+// exports.default = callbackError;
