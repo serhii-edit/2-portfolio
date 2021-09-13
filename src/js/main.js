9a37dc32c32163a9b3a7621e9ae5475804d71ebd
Serhii Yakymenko <64821812+serhii-edit@users.noreply.github.com>
2021-09-13 19:32:04 -0400

    Feat: add js modal open not yet work | not organized

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("Content Loaded");
+
+  var portfolio_1 = document.getElementsById("openCase_1");
+  var case_1 = document.getElementById("card_1");
+
+  function toggleModalA() {
+    portfolio_1.classList.toggle("portfolio-1");
+  }
+
+
+  // var case_1 = document.getElementById("case-2");
+  // var case_1 = document.getElementById("case-3");
+  // var case_1 = document.getElementById("case-4");
+  // var case_1 = document.getElementById("case-5");
+  // var case_1 = document.getElementById("case-6");
+
+  // var openCase_1 = document.getElementById("openCase_1");
+ 
+
+
+});
